3f991f4cd45ce909dc0020626201c21631ea52d9
jasson31 <jasson31@naver.com>
2020-10-30 03:24:32 +0900

    Police 및 Thief 싱글톤 제거

--- Assets/Scripts/Thief.cs
+++ Assets/Scripts/Thief.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Thief : SingletonBehaviour<Thief>
+public abstract class Thief
 {
     public abstract ThiefInfo[] InitialThiefPos();
     public abstract MoveInfo[] NextThiefPos();
